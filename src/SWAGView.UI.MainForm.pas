f7e7f2bf40f8689b06917391eac45811d47bc925
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2023-07-07 10:03:11 +0100

    Add new SWAGView project with icon & blank main form
    Project supports Windows 32 bit and 64 bit targets in both debug and
    release builds.
    
    Project uses the FMX framework.
    
    Project icon is default DelphiDabbler icon.

---
+++ src/SWAGView.UI.MainForm.pas
@@ -0,0 +1,23 @@
+unit SWAGView.UI.MainForm;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+
+type
+  TMainForm = class(TForm)
+  strict private
+  public
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.fmx}
+
+end.
+
---
+++ src/SWAGView.dpr
@@ -0,0 +1,17 @@
+﻿program SWAGView;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  SWAGView.UI.MainForm in 'SWAGView.UI.MainForm.pas' {MainForm},
+
+{$R *.res}
+
+begin
+  {$IFDEF DEBUG}
+  ReportMemoryLeaksOnShutdown := True;
+  {$ENDIF}
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
