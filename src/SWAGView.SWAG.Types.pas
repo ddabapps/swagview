0e120cfaf491b21737fe77537a3128ce3d16961d
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2024-08-13 19:21:04 +0100

    Add concept of a null record to TSWAGPacket
    Added rogue packet ID value of High(Cardinal) as marker for null
    TSWAGPacket records.
    Added record initialiser method to set packet ID to rogue value
    automatically on creation.
    Added new TSWAGPacket.IsNull method to test if a record is null.
    
    This done to enable using code to detect either a currently displayed
    packet whether if no packet has yet be displayed.

--- src/SWAGView.SWAG.Types.pas
+++ src/SWAGView.SWAG.Types.pas
@@ -30,29 +30,48 @@ type
   ///  <summary>Record that encapsulates the data that defines a SWAG packet
   ///  </summary>
   TSWAGPacket = record
-    ///  <summary>Number that uniquely identifies a SWAG packet.</summary>
-    ID: Cardinal;
-    ///  <summary>ID of SWAG category that packet belongs to.</summary>
-    Category: Cardinal;
-    ///  <summary>File name of packet in original SWAG archive.</summary>
-    FileName: string;
-    ///  <summary>Date and time packet was added to or updated in the SWAG
-    ///  archive.</summary>
-    DateStamp: TDateTime;
-    ///  <summary>SWAG packet title.</summary>
-    Title: string;
-    ///  <summary>Name(s) of author(s) of SWAG packet.</summary>
-    Author: string;
-    ///  <summary>Source code of SWAG packet.</summary>
-    ///  <remarks>Strictly speaking this is the text of the packet since not
-    ///  all packets are pure source code - some are text documents.</remarks>
-    SourceCode: string;
-    ///  <summary>Flag that indicates if SWAG packet is a text document
-    ///  (<c>True</c>) or is Pascal source code (<c>False</c>).</summary>
-    IsDocument: Boolean;
+    strict private
+      const NullMarker = High(Cardinal);
+    public
+      ///  <summary>Number that uniquely identifies a SWAG packet.</summary>
+      ID: Cardinal;
+      ///  <summary>ID of SWAG category that packet belongs to.</summary>
+      Category: Cardinal;
+      ///  <summary>File name of packet in original SWAG archive.</summary>
+      FileName: string;
+      ///  <summary>Date and time packet was added to or updated in the SWAG
+      ///  archive.</summary>
+      DateStamp: TDateTime;
+      ///  <summary>SWAG packet title.</summary>
+      Title: string;
+      ///  <summary>Name(s) of author(s) of SWAG packet.</summary>
+      Author: string;
+      ///  <summary>Source code of SWAG packet.</summary>
+      ///  <remarks>Strictly speaking this is the text of the packet since not
+      ///  all packets are pure source code - some are text documents.</remarks>
+      SourceCode: string;
+      ///  <summary>Flag that indicates if SWAG packet is a text document
+      ///  (<c>True</c>) or is Pascal source code (<c>False</c>).</summary>
+      IsDocument: Boolean;
+      ///  <summary>Record initialiser: flags record as null.</summary>
+      class operator Initialize(out Dest: TSWAGPacket);
+      ///  <summary>Checks if record is null.</summary>
+      function IsNull: Boolean;
   end;
 
 implementation
+
+{ TSWAGPacket }
+
+class operator TSWAGPacket.Initialize(out Dest: TSWAGPacket);
+begin
+  Dest.ID := NullMarker;
+end;
+
+function TSWAGPacket.IsNull: Boolean;
+begin
+  Result := ID = NullMarker;
+end;
 
 end.
 
